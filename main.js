5edc79b876acb288a152747eb340e171fa9e3160
MiliRisal <milirisal11@gmail.com>
2024-04-08 02:17:00 -0400

    created routes folder

--- main.js
+++ main.js
@@ -1,40 +1,29 @@
 const express = require("express");
-const path = require("path");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const Product = require("./models/Product");
+const connectDB = require("./mongodb");
+const productRoutes = require("./Routes/productRoutes");
+const userRoutes = require("./Routes/userRoutes");
+const commentsRoutes = require("./Routes/commentsRoutes");
+const cartRoutes = require("./Routes/cartRoutes");
+const orderRoutes = require("./Routes/orderRoutes");
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI;
 
-app.use(express.static(path.join(__dirname, "public")));
+// Middleware
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.once("open", () => {
-  console.log("Connected to MongoDB");
-});
+connectDB();
 
 // Routes
-app.get("/products", async (req, res) => {
-  try {
-    const products = await Product.find();
-    res.json(products);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-// Other routes for User, Comments, Cart, Order, etc.
+app.use("/products", productRoutes);
+app.use("/users", userRoutes);
+app.use("/comments", commentsRoutes);
+app.use("/cart", cartRoutes);
+app.use("/orders", orderRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
